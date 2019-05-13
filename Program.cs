4a15a23f4e3e7c28a48e6ea82ae218d30704edbf
dilancastillo <42076904+dilancastillo@users.noreply.github.com>
2019-05-13 14:07:19 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
